ad06f7772254abde92bbb008489073e830c389a1
AliMousa27 <jafar.myhero@gmail.com>
2024-07-15 15:21:21 +0200

    initial structural commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/bubbles.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/bubbles.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="background"></div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.background {
+  background: linear-gradient(40deg, #6c003e, #001152);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
