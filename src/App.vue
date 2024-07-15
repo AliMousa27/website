7812b86b5446d81a05ce91f19d09a6af61dac142
AliMousa27 <jafar.myhero@gmail.com>
2024-07-15 16:23:16 +0200

    added first circle

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,4 @@ export default {
 };
 </script>
 
-<style>
-html,
-body {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100%;
-}
-</style>
+<style></style>
--- src/components/bubbles.vue
+++ src/components/bubbles.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="background"></div>
+  <div class="background">
+    <div class="circle1"></div>
+    <div class="circle2"></div>
+    <div class="circle3"></div>
+    <div class="circle4"></div>
+    <div class="circle5"></div>
+  </div>
 </template>
 
 <script>
@@ -17,5 +23,25 @@ export default {
   position: absolute;
   width: 100%;
   height: 100%;
+  left: 0;
+  top: 0;
+}
+
+.circle1 {
+  width: 80%;
+  height: 80%;
+
+  background: radial-gradient(
+      circle at center,
+      rgba(0, 22, 222, 0.8) 0%,
+      rgba(0, 33, 5, 0) 35%
+    )
+    no-repeat;
+  mix-blend-mode: hard-light;
+  position: absolute;
+  opacity: 1;
+  top: calc(50% - 80% / 2);
+  left: calc(50% - 80% / 2);
+  transform-origin: center center;
 }
 </style>
