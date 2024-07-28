67531424e25993585344ceedf00f47f444a8028a
AliMousa27 <jafar.myhero@gmail.com>
2024-07-28 18:10:32 +0200

    made hero picture conform

--- src/components/about_me_components/hero_pictures.vue
+++ src/components/about_me_components/hero_pictures.vue
@@ -5,19 +5,17 @@
     style="transform: translateY(0px)"
   >
     <div class="col-12 text-center">
-      <img :src="require('@/assets/me.jpg')" class="img-fluid" id="first" />
-      <img id="second" :src="require('@/assets/view.jpg')" class="img-fluid" />
-      <img id="third" :src="require('@/assets/cat.jpg')" />
+      <img
+        v-for="(image, index) in images"
+        :key="index"
+        :src="image"
+        class="img-fluid"
+        :id="'image-' + index"
+      />
     </div>
   </div>
   <div v-else>
-    <CarouselSlides
-      :images="[
-        require('@/assets/me.jpg'),
-        require('@/assets/cat.jpg'),
-        require('@/assets/view.jpg'),
-      ]"
-    />
+    <CarouselSlides :images="images" />
   </div>
 </template>
 
@@ -46,6 +44,15 @@ export default {
   components: {
     CarouselSlides,
   },
+  data() {
+    return {
+      images: [
+        require("@/assets/view.jpg"),
+        require("@/assets/cat.jpg"),
+        require("@/assets/me.jpg"),
+      ],
+    };
+  },
 };
 </script>
 
@@ -63,26 +70,18 @@ img {
   }
 }
 
-#second {
+#image-0 {
   z-index: -1;
   animation: move_right 1s ease forwards;
   animation-delay: 750ms;
 }
 
-#third {
+#image-1 {
   z-index: -2;
   animation: move_left 750ms ease forwards;
   animation-delay: 500ms;
 }
 
-@keyframes move_vertically {
-  0% {
-    transform: translateY(-500px);
-  }
-  100% {
-    transform: translateY(0px);
-  }
-}
 @keyframes move_right {
   0% {
     transform: perspective(1000px) translateX(0px) translateZ(0px);
